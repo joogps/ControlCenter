6f46d0401eee627117a31606bbd84591be20e5d5
joogps <joogps@gmail.com>
2021-06-09 14:39:58 -0300

    Initial Commit

---
+++ ControlCenter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ControlCenter
+//
+//  Created by João Gabriel Pozzobon dos Santos on 09/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ControlCenter/ControlCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlCenterApp.swift
+//  ControlCenter
+//
+//  Created by João Gabriel Pozzobon dos Santos on 09/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ControlCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
